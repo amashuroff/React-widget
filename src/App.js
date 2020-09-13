459f9d4bbebb970242b7fe04f6dea4d11d9780c6
Aleksei <amashuroff@gmail.com>
2020-09-13 15:50:07 +0300

    finished navigation and routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from './components/Header';
 
 const items = [
   {
@@ -34,29 +36,30 @@ const options = [
   },
 ];
 
+
+
 export default () => {
-  // const [selected, setSelected] = useState(options[0]);
-  // const [showDropdown, setShowDropdown] = useState(true);
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      {/* <button
-        className="ui button"
-        onClick={() => setShowDropdown(!showDropdown)}
-      >
-        Toggle dropdown
-      </button>
-      {showDropdown ? (
+    <Header/>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
         <Dropdown
+          label="select a color"
           options={options}
           selected={selected}
           onSelectedChange={setSelected}
-          label={'Select a color'}
-        >
-          <p style={{ color: selected.value }}>The text is {selected.value}</p>
-        </Dropdown>
-      ) : null} */}
-
-      <Translate  />
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
